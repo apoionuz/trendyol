f5c4dbabbfc861cf43282f3588a010c47b4ce774
Abdullah Yuksel <42186592+apoionuz@users.noreply.github.com>
2022-10-19 04:04:45 +0300

    Update trendyol.py

--- trendyol.py
+++ trendyol.py
@@ -1,37 +1,64 @@
+#!/usr/bin/python
+
 from requests import get
 from bs4 import BeautifulSoup
 from pandas import DataFrame, concat
-from time import sleep
+import threading
+import time
 
-print("""required libraries:
-requests
-bs4
-pandas
-""")
 
-url = input("Enter URL: ")
-page_num = int(input("Enter how many pages you want to scrape (integer): "))
-time = float(input("Enter how many seconds to sleep between GET requests (float): "))
+class TrendyolScrapper:
+    def __init__(self):
+        self.page_table = DataFrame(columns=("brand", "model", "price"))
 
-if "?pi=" in url:
-    url = url[:url.find("?pi=")]
+    def download_pages(self, page_url: str, page_count: int):
+        threads = []
 
-table = DataFrame(columns=("brand","model","price"))
-for rep in range(1, page_num + 1):
-    soup = BeautifulSoup(get(url + "?pi=" + str(rep)).content, "html.parser")
-    products = soup.find("div", {"class" : "prdct-cntnr-wrppr"}).find_all("div", {"class" : "prdct-desc-cntnr-wrppr"})
-    for num, value in enumerate(products):
-        brand = value.find("span", {"class":"prdct-desc-cntnr-ttl"}).text
-        model = value.find("span", {"class":"prdct-desc-cntnr-name"}).text
-        price = value.find("div", {"class":"prc-box-dscntd"}).text
-        table = concat([table, DataFrame([[brand, model, price]], columns=("brand","model","price"))])
-    print("Page " + str(rep))
-    sleep(time)
+        try:
+            for page_index in range(1, page_count + 1):
+                threads.append(threading.Thread(target=self.download_page, args=(page_url, page_index,)))
+        except:
+            print("Unable to create threads")
 
-print("All pages are scrapped")
+        try:
+            for thread in threads:
+                thread.start()
+        except:
+            print("Unable to start threads")
 
-table = table.reset_index()
-table.to_csv("trendyol.csv")
+        try:
+            for thread in threads:
+                thread.join()
+        except:
+            print("Unable to join threads")
 
-print("CSV file is created")
-input("Please Enter to Exit...")
+    def download_page(self, page_url: str, page_index: int):
+        raw_page = get(page_url + "?pi=" + str(page_index))
+        soup = BeautifulSoup(raw_page.content, "html.parser")
+
+        products = soup \
+            .find("div", {"class": "prdct-cntnr-wrppr"}) \
+            .find_all("div", {"class": "prdct-desc-cntnr-wrppr"})
+
+        for num, value in enumerate(products):
+            brand = value.find("span", {"class": "prdct-desc-cntnr-ttl"}).text
+            model = value.find("span", {"class": "prdct-desc-cntnr-name"}).text
+            price = value.find("div", {"class": "prc-box-dscntd"}).text
+            self.page_table = concat([self.page_table, DataFrame([[brand, model, price]],
+                                                             columns=("brand", "model", "price"))])
+
+    def save(self):
+        self.page_table = self.page_table.reset_index()
+        self.page_table.to_csv("trendyol.csv")
+
+
+if __name__ == "__main__":
+    page_url = input("Enter Product Url: ")
+    page_num = int(input("Enter how many pages of data you want (integer): "))
+
+    scrapper = TrendyolScrapper()
+    scrapper.download_pages(page_url, page_num)
+    scrapper.save()
+
+    print("Csv file is created")
+    input("Please Enter to Exit...")
